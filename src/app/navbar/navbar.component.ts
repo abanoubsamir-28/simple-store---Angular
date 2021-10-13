a0f9e047185bf823112685ee5dbaa1a1724f6ce0
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2021-10-13 18:30:45 +0200

    edit navbar to add card item

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  counter:any ;
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
