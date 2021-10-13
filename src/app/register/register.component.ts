5ad0b5a9c7e629cba3752412ebe1a3bab94594ef
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2021-10-13 17:13:41 +0200

    edit register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm !: FormGroup ;
+  constructor(private router: Router , private fb : FormBuilder) { }
 
-  constructor() { }
-
+  toLogin() {
+    this.router.navigate(['login']);
+  }
+  submitRegister() {
+    console.log(this.registerForm.controls.username)
+  }
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name : [null,[Validators.required]] ,
+      email :[null,[Validators.required , Validators.email]] ,
+      username:[null,[Validators.required , Validators.pattern('/^\S*$/')]] ,
+      password:[null,[Validators.required , Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$')]] ,
+      confirmPassword:[null,[Validators.required]]
+    })
   }
 
 }
